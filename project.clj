888b19537c690e3a53ee71eb158b8dd350929ef6
John Stevenson (jr0cket) <john@jr0cket.co.uk>
2012-02-11 11:59:03 +0000

    Initial setup and defined my first, rather annoying, instrument

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject tutorial "1.0.0-SNAPSHOT"
+  :description "A simple overture example"
+  :dependencies [[org.clojure/clojure "1.3.0"]
+                 [overtone "0.6.0"]])
+
+
+
---
+++ src/tutorial/core.clj
@@ -0,0 +1,8 @@
+(ns tutorial.core)
+
+  ; (use 'overtone.core) ; to connect to the super collider, the external overtone server
+; (use 'overtone.live)  ; to connect to the internal overtone server
+
+; Define a very boring instrument
+(definst annoying-tone [] (saw 220))
+
