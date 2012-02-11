ce45a70982d49d1bcc36c3c3f2f1976f5ff55c8d
John Stevenson (jr0cket) <john@jr0cket.co.uk>
2012-02-11 12:30:25 +0000

    Add Overtone namespace to our code

--- src/tutorial/core.clj
+++ src/tutorial/core.clj
@@ -1,4 +1,5 @@
-(ns tutorial.core)
+(ns tutorial.core
+  (:use overtone.live))
 
   ; (use 'overtone.core) ; to connect to the super collider, the external overtone server
 ; (use 'overtone.live)  ; to connect to the internal overtone server
